94702d41039f430f4ab588719189ca6c436607e8
crackingme <sunyong19890@163.com>
2017-09-01 16:17:27 +0800

    添加config

--- browser/browser1/app_config.h
+++ browser/browser1/app_config.h
@@ -5,23 +5,35 @@
 class AppConfig
 {
 
-
-
+public:
+	
+	string inifile;
 	string regkey;
+	string        inipath;
+private:
+	string m_ask_before_close;
+
+
 public:	
 	AppConfig();
 	void setRegKey(const string& keyname);
+	void setAskBeforeClose(const string& value);
+	bool IfAskBeforeClose();
+	string getAskBeforeClose();
+	string getConfigFile();
 	virtual ~AppConfig();
 };
 
 
-class BrowserApp:public AppConfig
+
+class BrowserApp :public AppConfig
 {
 	CppSQLite3DB db;//关于browser的数据
 	string       m_appdir;
 	CFavorManager* m_favor;
 	CHistoryMgr*   m_history;
 	CRegKey        m_regedit;
+	
 public:
 	BrowserApp();
 	~BrowserApp();
